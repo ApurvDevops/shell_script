9db7ba9c82fd7cbe47175b3dbdb250712c686ca2
ApurvDevops <apurv.samadder@gmail.com>
2023-07-31 09:55:34 +0200

    adding interactive script

---
+++ scripts/interactive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "What is your First Name"
+read first_name
+echo "What is your last name"
+read last_name
+echo "------------"
+echo "Hello user $first_name $last_name, you have tested successfully"
+echo "------------"
+
